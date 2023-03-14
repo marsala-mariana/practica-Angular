8a6bc448cbf0cc5424de321353c4a3f70b1caee5
Mariana Marsala <stephaniemarsala08gmail.com>
2023-03-14 08:46:35 -0300

    terminado

--- src/app/pipes/marcket-name.pipe.ts
+++ src/app/pipes/marcket-name.pipe.ts
@@ -5,15 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MarcketNamePipe implements PipeTransform {
   marckets = [
-    { id: ' amazon_price ', name: 'Amazon' },
-    { id: ' cardmarket_price ', name: 'Cardmarket' },
-    { id: ' coolstuffinc_price ', name: 'Coolstuff Inc' },
-    { id: ' ebay_price ', name: 'Ebay' },
-    { id: ' tcgplayer_price ', name: 'TCG Player' },
+    { id: 'amazon_price', name: 'Amazon' },
+    { id: 'cardmarket_price', name: 'Cardmarket' },
+    { id: 'coolstuffinc_price', name: 'Coolstuff Inc' },
+    { id: 'ebay_price', name: 'Ebay' },
+    { id: 'tcgplayer_price', name: 'TCG Player' },
   ];
 
   transform(value: string): string {
     const market = this.marckets.find((m) => m.id === value);
+
     return market?.name || '';
   }
 }
